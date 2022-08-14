428e80686aec327fd548c0f69720e1fe3dfa492a
P-Yevhenii <py@atticlab.net>
2022-08-14 20:26:01 +0300

    Added simple express server

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# astroport
-astroport contracts 
-start index.html
+# Contracts version
+
+# Setup dependencies
+- npm install
+
+# Run server
+- node src/server.js
+
+# Go to the https://localhost:15000 to view frontend part
---
+++ src/server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('.'));
+
+app.get('/values', (req, res) => {
+    let values = [
+        {
+            "address": "terra19t8ffmz6q2rdm3rllyksd6sex6n650a4anttzzvz9mf8mqr4nkrq44cyu6",
+            "localName": "staking_address",
+            "localVersion": "1.0.1",
+            "deployedName": "astroport-staking",
+            "deployedVersion": "1.0.0"
+        },
+        {
+            "address": "terra19t8ffmz6q2rdm3rllyksd6sex6n650a4anttzzvz9mf8mqr4nkrq44cyu6",
+            "localName": "staking_address",
+            "localVersion": "1.0.1",
+            "deployedName": "astroport-staking",
+            "deployedVersion": "1.0.0"
+        },
+        {
+            "address": "terra19t8ffmz6q2rdm3rllyksd6sex6n650a4anttzzvz9mf8mqr4nkrq44cyu6",
+            "localName": "staking_address",
+            "localVersion": "1.0.1",
+            "deployedName": "astroport-staking",
+            "deployedVersion": "1.0.0"
+        },
+        {
+            "address": "terra19t8ffmz6q2rdm3rllyksd6sex6n650a4anttzzvz9mf8mqr4nkrq44cyu6",
+            "localName": "staking_address",
+            "localVersion": "1.0.1",
+            "deployedName": "astroport-staking",
+            "deployedVersion": "1.0.0"
+        },
+        {
+            "address": "terra19t8ffmz6q2rdm3rllyksd6sex6n650a4anttzzvz9mf8mqr4nkrq44cyu6",
+            "localName": "staking_address",
+            "localVersion": "1.0.1",
+            "deployedName": "astroport-staking",
+            "deployedVersion": "1.0.0"
+        }
+    ];
+
+    return res.send(values);
+});
+
+const server = app.listen(15000, function () {
+
+    const host = server.address().address;
+    const port = server.address().port;
+
+    console.log('Express app listening at https://%s:%s', host, port)
+
+});
