a3258733f424f98fd6cfd754d60eb90ead355a37
anaragamovich <108760477+anaragamovich@users.noreply.github.com>
2022-08-23 23:53:34 +0300

    add translate

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const select = document.querySelector('select');
+const allLang = ['en', 'ua', 'ru'];
+
+select.addEventListener('change', changeURLLanguage);
+function changeURLLanguage(){
+	let lang = select.value;
+	location.href = window.location.pathname + '#'+lang
+	location.reload();
+}
+
+function changeLanguage(){
+	let hash = window.location.hash;
+	hash = substr(1);
+	console.log(hash);
+	if (!allLang.includes(hash)){
+	location.href = window.location.pathname + '#en'
+	location.reload();
+	}
+	select.value = hash;
+	
+	for (let key in langArr) {
+		let elem = document.querySelector('.lng-' + key);
+		if (elem) {
+			elem.innerHTML = langArr[key][hash];
+		}
+	}
+}
---
+++ js/lang.js
@@ -0,0 +1,52 @@
+const langArr = {
+	"lng-title" : {
+		"en" : "contract version",
+		"ua" : "версії контракту",
+		"ru" : "версии контракта",
+	},
+	"lng-main" : {
+		"en" : "main title",
+		"ua" : "заголовок",
+		"ru" : "заголовок",
+	},
+	"lng-name" : {
+		"en" : "name",
+		"ua" : "ім'я",
+		"ru" : "имя",
+	},
+	"lng-variety" : {
+		"en" : "variety",
+		"ua" : "варіант",
+		"ru" : "вариант",
+	},
+	"lng-projekt-one" : {
+		"en" : "project one",
+		"ua" : "проект один",
+		"ru" : "проэкт один",
+	},
+	"lng-projekt-two" : {
+		"en" : "project two",
+		"ua" : "проект два",
+		"ru" : "проэкт два",
+	},
+	"lng-projekt-three" : {
+		"en" : "thirty part project",
+		"ua" : "проект з тридцяти частин",
+		"ru" : "проэкт из тридцати частей",
+	},
+	"lng-test-one" : {
+		"en" : "test one",
+		"ua" : "тест один",
+		"ru" : "тест один",
+	},
+	"lng-test-two" : {
+		"en" : "test two",
+		"ua" : "тест два",
+		"ru" : "тест три",
+	},
+	"lng-test-three" : {
+		"en" : "test three",
+		"ua" : "тест три",
+		"ru" : "тест три",
+	},
+}
